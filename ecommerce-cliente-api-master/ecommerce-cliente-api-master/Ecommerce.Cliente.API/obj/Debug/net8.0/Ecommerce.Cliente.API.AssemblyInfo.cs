f8655fb9dbeef2d4e700779c3dab2644dbbd60fa
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-29 22:47:38 -0300

    ajustando erro

--- ecommerce-cliente-api-master/ecommerce-cliente-api-master/Ecommerce.Cliente.API/obj/Debug/net8.0/Ecommerce.Cliente.API.AssemblyInfo.cs
+++ ecommerce-cliente-api-master/ecommerce-cliente-api-master/Ecommerce.Cliente.API/obj/Debug/net8.0/Ecommerce.Cliente.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Cliente.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57cc27e6f888ec81915d39ebc3af6c77ed8fedb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a459da60170a216efa2f73213aaeb1d3994187")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Cliente.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Cliente.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ecommerce-cliente-api-master/ecommerce-cliente-api-master/Ecommerce.Cliente.Domain/Entities/EstatisticaEntity.cs
+++ ecommerce-cliente-api-master/ecommerce-cliente-api-master/Ecommerce.Cliente.Domain/Entities/EstatisticaEntity.cs
@@ -9,8 +9,8 @@ namespace Ecommerce.Estatistica.Domain.Entities
 		//[Key] - Utilizado no Entity para indicar o campo chave (Identificador) da base de dados (Tabela)
 		[BsonId]
 		public ObjectId Id { get; set; }
-		public int MediaCrescimento { get; set; } = int.Empty;
-		public int CrescimentoMensal { get; set; } = int.Empty;
-		public int Receita { get; set; } = int.Empty;
+		public int MediaCrescimento { get; set; } 
+		public int CrescimentoMensal { get; set; }
+		public int Receita { get; set; }
 	}
 }
